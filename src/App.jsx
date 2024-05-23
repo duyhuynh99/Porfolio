d1d2d1c428449ecce635b4d5f332a6061933bf08
Trường Duy <truongduy0412bku@gmail.com>
2024-05-23 16:47:12 +0700

    update startcanvas v1.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import  { lazy, Suspense } from 'react';
 import { StarsCanvas } from './components';
 // Import các component cần lazy load
-const StarsCanvasLazy = lazy(() => import('./components/canvas/Stars'));
 const Navbar = lazy(() => import('./components/Navbar'));
 const Hero = lazy(() => import('./components/Hero'));
 const About = lazy(() => import('./components/About'));
@@ -32,14 +31,14 @@ function App() {
             <Suspense fallback={null}>
               <Experience />
               <Tech />
-              <StarsCanvasLazy size={0.0015} item={600} />
+              <StarsCanvas size={0.0015} item={600} />
             </Suspense>
           </div>
           <div className='relative z-0'>
             <Suspense fallback={null}>
               <Works />
               <Contact />
-              <StarsCanvasLazy size={0.0015} item={800} />
+              <StarsCanvas size={0.0015} item={800} />
             </Suspense>
           </div>
         </div>
